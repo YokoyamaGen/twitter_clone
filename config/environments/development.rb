801c561d62deef6935edb9b76281fe287f4e9fb4
YokoyamaGen <g.yokoyama.0616@gmail.com>
2024-02-11 20:54:15 +0900

    ブラウザのリロードでcssが読み込まれるように修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener_web
+
+  config.assets.debug = true
+  config.assets.compile = true
+  config.assets.digest = false
 end
