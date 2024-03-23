15cdff32ee7887123fa6928af8d4758dbc017e22
YokoyamaGen <g.yokoyama.0616@gmail.com>
2024-03-23 14:03:53 +0900

    リファクタリング

--- app/models/concerns/common_notification.rb
+++ app/models/concerns/common_notification.rb
@@ -15,15 +15,7 @@ module CommonNotification
                  'ポストにいいねしました'
                end
 
-      NotificationMailer.send_mail(action, user_email, user_name).deliver_now
+      NotificationMailer.send_mail(action, tweet.user_email, notification.event.user.name).deliver_now
     end
-  end
-
-  def user_email
-    tweet.user.email
-  end
-
-  def user_name
-    notification.event.user.name
   end
 end
